64e3058680a10fce60e4dbe152075098357b0709
june.k.lara@gmail.com <june.k.lara@gmail.com>
2016-12-15 23:39:03 -0600

    fixed connection issue

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = 3000;
-
 var app = express();
+var port = process.env.PORT || 3000;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
